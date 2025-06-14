1ee47ea46fbb68ca55f424b8121b5be2bb63999a
Chinmay M <chinmaym6166@gmail.com>
2025-06-15 03:29:11 +0530

    Database COnnected Success

---
+++ backend/src/config/database.js
@@ -0,0 +1,25 @@
+import {Pool} from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+    user:process.env.USER,
+    host:process.env.HOST,
+    database:process.env.DATABASE,
+    password:process.env.PASSWORD,
+    port:process.env.DB_PORT
+});
+
+const connectToDatabase = async (e)=>{
+    try{
+        await pool.connect();
+        console.log("Database Connected successfully!")
+    }
+    catch(error){
+        console.log("Problem connecting to Database")
+        process.exit(1);
+    }
+};
+
+export default connectToDatabase;
