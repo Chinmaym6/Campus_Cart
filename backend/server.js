f35b6e2b3e5f6578ac10b8f9f215b5051cf7df4f
Chinmay M <chinmaym6166@gmail.com>
2025-06-15 03:29:11 +0530

    Database COnnected Success

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from "express";
+import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectToDatabase from "./src/config/database.js";
 
 
+dotenv.config();
 const app=express();
-const port=3000;
+const port=process.env.PORT || 5000;
+
+app.use(cors({ origin: 'http://localhost:3000'}));
+app.use(express.json());
+
+
+connectToDatabase();
+
 
 app.listen(port,()=>{
     console.log('Server is running at http://localhost:5000/')
